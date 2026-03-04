b3147cefa746cd0e5fd136672aa5610668c22bb0
Ladina <ladina.lozza@bluewin.ch>
2026-03-04 09:57:45 +0100

    anpassung

--- views/home.py
+++ views/home.py
@@ -5,16 +5,14 @@ st.title("Kalorienbedarf Rechner")
 st.badge("Verstehen Sie Ihren Kalorienbedarf und treffen Sie informierte Entscheidungen für Ihre Gesundheit!", color="green")
 
 """
-Diese App wurde von folgenden Personen entwickelt:
-- Giuliana Cerullo (cerulgiu@students.zhaw.ch)
-- Ladina Lozza (lozzalad@students.zhaw.ch)
-
-
 Die App berechnet Ihren Kalorienbedarf basierend auf den folgenden Faktoren:
 - Alter
 - Geschlecht
 - Gewicht
 - Grösse
 - Aktivitätslevel
-"""
 
+Diese App wurde von folgenden Personen entwickelt:
+- Giuliana Cerullo (cerulgiu@students.zhaw.ch)
+- Ladina Lozza (lozzalad@students.zhaw.ch)
+"""

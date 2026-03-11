645318bb3c64c1cd9f7c0634b2ddcc58c00b8ecc
Ladina <ladina.lozza@bluewin.ch>
2026-03-11 09:04:31 +0100

    pandas

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import pandas as pd  # --- NEW CODE: add pandas to the imports ---
+import streamlit as st
+
 # --- NEW CODE: initialize empty data frame if not already present ---
 if 'data_df' not in st.session_state:
     st.session_state['data_df'] = pd.DataFrame()
